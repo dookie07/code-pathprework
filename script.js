f80e122557b87954500bd8a84f01c3c6609d6bc9
Glitch (glitch-blank-website) <none>
2022-03-31 17:22:39 +0000

    🏇🌛 Checkpoint ./script.js:43409440/5687 ./style.css:43409440/3291 ./index.html:43409440/2219

--- script.js
+++ script.js
@@ -1,4 +1,62 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
 
-console.log("Hello, world!");
+
+var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var progress = 0; 
+var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5;
+
+
+function startGame(){
+    progress = 0;
+    gamePlaying = true;
+    document.getElementById("startBtn").classList.add("hidden");
+    document.getElementById("stopBtn").classList.remove("hidden");
+}
+
+function stopGame(){
+    gamePlaying = false;
+    document.getElementById("startBtn").classList.remove("hidden");
+    document.getElementById("stopBtn").classList.add("hidden");
+}
+
+// Sound Synthesis Functions
+const freqMap = {
+    1: 261.6,
+    2: 329.6,
+    3: 392,
+    4: 466.2
+  }
+  function playTone(btn,len){ 
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    context.resume()
+    tonePlaying = true
+    setTimeout(function(){
+      stopTone()
+    },len)
+  }
+  function startTone(btn){
+    if(!tonePlaying){
+      context.resume()
+      o.frequency.value = freqMap[btn]
+      g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+      context.resume()
+      tonePlaying = true
+    }
+  }
+  function stopTone(){
+    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+    tonePlaying = false
+  }
+  
+  // Page Initialization
+  // Init Sound Synthesizer
+  var AudioContext = window.AudioContext || window.webkitAudioContext 
+  var context = new AudioContext()
+  var o = context.createOscillator()
+  var g = context.createGain()
+  g.connect(context.destination)
+  g.gain.setValueAtTime(0,context.currentTime)
+  o.connect(g)
+  o.start(0)
